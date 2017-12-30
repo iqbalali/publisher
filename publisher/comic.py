983eb75432359267cc89c2dd9bd890e45f4587a1
Iqbal Ali <iqbala@ttl001836.lan>
2017-12-30 19:47:45 +0000

    Added cover support

--- publisher/comic.py
+++ publisher/comic.py
@@ -85,5 +85,5 @@ class Publisher:
         self.dir_create(PUBLISH_FOLDER)
         os.system('gs           -o '+PUBLISH_FOLDER+'/'+self.PROJ_NAME+'.cs_cover.pdf           -sDEVICE=pdfwrite           -dPDFSETTINGS=/printer           -f '+SRC_FILE)
         
-        os.system('gs           -o '+PUBLISH_FOLDER+'/'+self.PROJ_NAME+'.front.pdf           -sDEVICE=pdfwrite           -dPDFSETTINGS=/printer           -dFIXEDMEDIA           -dDEVICEWIDTHPOINTS=504              -dDEVICEHEIGHTPOINTS=738             -c "<</PageOffset [-'+str(513+COVER_OFFSET)+' 0]>> setpagedevice"           -f '+PUBLISH_FOLDER+'/'+self.PROJ_NAME+'.cs_cover.pdf')
+        os.system('gs           -o '+PUBLISH_FOLDER+'/'+self.PROJ_NAME+'.front.pdf           -sDEVICE=pdfwrite           -dPDFSETTINGS=/printer           -dFIXEDMEDIA           -dDEVICEWIDTHPOINTS=504              -dDEVICEHEIGHTPOINTS=738             -c "<</PageOffset [-'+str(513+COVER_OFFSET)+' 0]>> setpagedevice"           -f '+SRC_FILE)
 
